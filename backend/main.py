6c122a3aaae64a75bb36cccbd1c2a6c74998f23e
nairjayashreen-cell <nairjayashreen@gmail.com>
2026-05-27 13:29:39 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from google.cloud import vision
 import requests
 import os
+import json
+
+# =========================
+# CREATE GOOGLE JSON FILE
+# =========================
+
+if os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON"):
+
+    credentials_data = json.loads(
+        os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+    )
+
+    with open("google-vision.json", "w") as f:
+        json.dump(credentials_data, f)
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-vision.json"
+
+# =========================
+# FASTAPI
+# =========================
 
 app = FastAPI()
 
@@ -14,12 +35,16 @@ app.add_middleware(
 )
 
 # =========================
+# GOOGLE VISION CLIENT
+# =========================
+
+vision_client = vision.ImageAnnotatorClient()
+
+# =========================
 # APIFY SETTINGS
 # =========================
 
 APIFY_TOKEN = "apify_api_tdIZP0fTwGtGbxxIbSQef5SFrJ34rw4oFhbs"
-
-# Replace with your Dataset ID
 DATASET_ID = "xYIBUaWUK5ie0sxpg"
 
 # =========================
@@ -31,6 +56,27 @@ def home():
     return {
         "status": "AI Visual Threat Monitoring API Running"
     }
+
+# =========================
+# LOGO DETECTION
+# =========================
+
+def detect_logos(image_url):
+
+    image = vision.Image()
+
+    image.source.image_uri = image_url
+
+    response = vision_client.logo_detection(image=image)
+
+    logos = response.logo_annotations
+
+    detected = []
+
+    for logo in logos:
+        detected.append(logo.description)
+
+    return detected
 
 # =========================
 # LIVE SCAN
@@ -49,20 +95,26 @@ def scan():
 
     for item in data:
 
+        image_url = item.get("displayUrl", "")
+
+        detected_logos = []
+
         try:
-
-            results.append({
-                "platform": "Instagram",
-                "username": item.get("ownerUsername", "unknown"),
-                "url": item.get("url", ""),
-                "brand": "Detected Brand",
-                "score": "96%",
-                "risk": "Critical",
-                "ocr": item.get("caption", "")[:120],
-                "time": item.get("timestamp", "recent")
-            })
-
+            detected_logos = detect_logos(image_url)
         except:
             pass
+
+        results.append({
+
+            "platform": "Instagram",
+            "username": item.get("ownerUsername", "unknown"),
+            "url": item.get("url", ""),
+            "brand": ", ".join(detected_logos),
+            "score": "96%",
+            "risk": "Critical",
+            "ocr": item.get("caption", "")[:120],
+            "time": item.get("timestamp", "recent")
+
+        })
 
     return results
